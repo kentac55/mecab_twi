08cbbc8654d05bd6d99f49d0186538d130dc2062
Kenta Fukushima <dev.kentac55@gmail.com>
2016-03-30 16:24:17 +0900

    first commit

---
+++ lib/build.sbt
@@ -0,0 +1,9 @@
+name := "mecaSca"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+scalacOptions ++= Seq("-deprecation", "-feature", "-unchecked", "-Xlint")
+
+libraryDependencies += "org.twitter4j" % "twitter4j-core" % "4.0.4"
---
+++ src/main/scala/mecasca.scala
@@ -0,0 +1,11 @@
+/**
+  * Created by kentac55 on 16/03/30.
+  */
+
+import org.chasen.mecab._
+
+object mecasca extends App{
+  System.loadLibrary("MeCab")
+  val tagger = new Tagger("-d /usr/lib/mecab/dic/mecab-ipadic-neologd/ -O wakati")
+  println(tagger.parse("10日放送の「中居正広のミになる図書館」（テレビ朝日系）で、SMAPの中居正広が、篠原信一の過去の勘違いを明かす一幕があった。."))
+}
